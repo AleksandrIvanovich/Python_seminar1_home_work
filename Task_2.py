a1b7ecfaeef0f121a00ed2dd17602793a958f2ea
Aleksandr <fomichov73@mail.ru>
2022-11-26 17:08:23 +0300

    Added the solution task_2

--- Task_2.py
+++ Task_2.py
@@ -1,4 +1,10 @@
-word = '335.099'
-print(word[2:])
-print(word[:1])
-print(word[:4])
+# Напишите программу для проверки истинности утверждения:
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# Нужно написать таблицу истинности.
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == (not x and not y and not z):
+                condition = (not (x or y or z) == (not x and not y and not z))
+                print(f'{x}\t{y}\t{z}\t{condition}')
